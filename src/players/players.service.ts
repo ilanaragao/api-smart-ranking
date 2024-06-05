03768f038c2b5c58ce6af43d9abea7cfb9cad852
Ilan Aragão <ilan_aragao@hotmail.com>
2024-06-05 18:55:03 -0300

    feat: implements get and delete requests

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
 import { Player } from './interfaces/player.interface';
 import { v4 as uuid } from 'uuid';
@@ -8,8 +8,41 @@ export class PlayersService {
   private readonly logger = new Logger(PlayersService.name);
   private players: Player[] = [];
 
+  async getAllPlayers(): Promise<Player[]> {
+    return this.players;
+  }
+
+  async getPlayerByEmail(email: string): Promise<Player> {
+    const playerFound = this.players.find((player) => player.email === email);
+
+    if (!playerFound) {
+      throw new NotFoundException(`Player with e-mail ${email} not found`);
+    }
+
+    return playerFound;
+  }
+
   async createOrUpdatePlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
-    this.createPlayer(createPlayerDto);
+    const { email } = createPlayerDto;
+    const playerFound = this.players.find((player) => player.email === email);
+
+    if (playerFound) {
+      this.updatePlayer(playerFound, createPlayerDto);
+    } else {
+      this.createPlayer(createPlayerDto);
+    }
+  }
+
+  async deletePlayer(email: string): Promise<void> {
+    const playerFound = this.players.find((player) => player.email === email);
+
+    if (!playerFound) {
+      throw new NotFoundException(`Player with e-mail ${email} not found`);
+    }
+
+    this.players = this.players.filter(
+      (player) => player.email !== playerFound.email,
+    );
   }
 
   private createPlayer(createPlayerDto: CreatePlayerDto): void {
@@ -27,5 +60,13 @@ export class PlayersService {
 
     this.logger.log(`createOrUpdatePlayer: ${JSON.stringify(player)}`);
     this.players.push(player);
+  }
+
+  private updatePlayer(
+    playerFound: Player,
+    createPlayerDto: CreatePlayerDto,
+  ): void {
+    const { name } = createPlayerDto;
+    playerFound.name = name;
   }
 }
